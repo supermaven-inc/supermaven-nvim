26f5234ace6461544466db7e8823c55521dee1fa
AlejandroSuero <asuerome@gmail.com>
2024-05-15 12:18:01 +0200

    fix(setup): modified config to `config = true` and `require(...).setup()`
    - [x] sets up with default configuration on `config = true`.
    - [x] sets up with default configuration on `config = function()
    require("supermaven-nvim").setup()`.
    
    > [!NOTE]
    >
    > Added types for `lua/supermaven/config.lua` and
    `lua/supermaven/init.lua`
    > for better developer experience with types.

--- lua/supermaven-nvim/config.lua
+++ lua/supermaven-nvim/config.lua
@@ -1,17 +1,40 @@
+---@class SuperMavenKeymap
+---@field accept_suggestion string: The keymap to accept the suggestion.
+---@field clear_suggestion string: The keymap to clear the suggestion.
+---@field accept_word string: The keymap to accept the word under the cursor.
+
+---@class SuperMavenColor
+---@field suggestion_color string|nil: The color of the suggestion.
+---@field cterm string|nil: The color of the suggestion in the terminal.
+
+---@class SuperMavenConfig
+---@field keymaps table<string, string>: The keymaps to use for the plugin.
+---@field ignore_filetypes table<string, string>: The filetypes to ignore.
+---@field color SuperMavenColor|nil: The color of the suggestion.
+
 local default_config = {
-  keymaps = {
-    accept_suggestion = "<Tab>",
-    clear_suggestion = "<C-]>",
-    accept_word = "<C-j>",
-  },
-  ignore_filetypes = {},
+	keymaps = {
+		accept_suggestion = "<Tab>",
+		clear_suggestion = "<C-]>",
+		accept_word = "<C-j>",
+	},
+	ignore_filetypes = {},
 }
 
 M = {}
 
+---@class SuperMavenConfig
+---@field default_config SuperMavenConfig: The default configuration.
+M.default_config = default_config
+
+---@class SuperMavenConfig
+---@field setup_config function: Sets up the configuration.
+--- Sets up the configuration with the default values and the given custom values.
+---@param args SuperMavenConfig|nil
+---@return SuperMavenConfig
 M.setup_config = function(args)
-  local config = vim.tbl_deep_extend("force", default_config, args)
-  return config
+	local config = vim.tbl_deep_extend("force", {}, default_config, args or {})
+	return config
 end
 
 return M
--- lua/supermaven-nvim/init.lua
+++ lua/supermaven-nvim/init.lua
@@ -2,35 +2,64 @@ local binary = require("supermaven-nvim.binary.binary_handler")
 local completion_preview = require("supermaven-nvim.completion_preview")
 local u = require("supermaven-nvim.util")
 local listener = require("supermaven-nvim.document_listener")
-local config = require("supermaven-nvim.config")
+local supermave_config = require("supermaven-nvim.config")
 
+---@class SuperMaven
 M = {}
 
+---@class SuperMavenConfig
+---@field config SuperMavenConfig
+M.config = {}
+
+---@class SuperMaven
+---@field setup function
+--- Sets up the plugin with the given a custom configuration (args)
+--- or the default configuration.
+---
+--- The default configuration is:
+---
+--- ```lua
+--- require("supermaven-nvim").setup({
+---   keymaps = {
+---     accept_suggestion = "<Tab>",
+---     clear_suggestion = "<C-]>",
+---     accept_word = "<C-j>",
+---   },
+---   ignore_filetypes = {},
+--- })
+--- ```
+---
+---Or you can pass a custom configuration:
+---
+---[link to the configuration section](https://github.com/supermaven-nvim/supermaven-nvim/blob/main/lua/supermaven-nvim/config.lua)
+---
+---@see SuperMavenConfig
+---@param args SuperMavenConfig
 M.setup = function(args)
-  local config_settings = config.setup_config(args)
-  if config_settings.keymaps.accept_suggestion ~= nil then
-    local accept_suggestion_key = config_settings.keymaps.accept_suggestion
+  M.config = supermave_config.setup_config(args)
+  if M.config.keymaps.accept_suggestion ~= nil then
+    local accept_suggestion_key = M.config.keymaps.accept_suggestion
     vim.keymap.set('i', accept_suggestion_key, completion_preview.on_accept_suggestion, { noremap = true, silent = true })
   end
 
-  if config_settings.keymaps.accept_word ~= nil then
-    local accept_word_key = config_settings.keymaps.accept_word
+  if M.config.keymaps.accept_word ~= nil then
+    local accept_word_key = M.config.keymaps.accept_word
     vim.keymap.set('i', accept_word_key, completion_preview.on_accept_suggestion_word, { noremap = true, silent = true })
   end
 
-  if config_settings.keymaps.clear_suggestion ~= nil then
-    local clear_suggestion_key = config_settings.keymaps.clear_suggestion
+  if M.config.keymaps.clear_suggestion ~= nil then
+    local clear_suggestion_key = M.config.keymaps.clear_suggestion
     vim.keymap.set('i', clear_suggestion_key, completion_preview.on_dispose_inlay, { noremap = true, silent = true })
   end
-  binary:start_binary(config_settings.ignore_filetypes or {})
+  binary:start_binary(M.config.ignore_filetypes or {})
 
-  if config_settings.color and config_settings.color.suggestion_color and config_settings.color.cterm then
+  if M.config.color and M.config.color.suggestion_color and M.config.color.cterm then
     vim.api.nvim_create_autocmd({"VimEnter", "ColorScheme"}, {
       pattern = "*",
-      callback = function(event)
-        vim.api.nvim_set_hl(0, "SupermavenSuggestion", { 
-          fg = config_settings.color.suggestion_color,
-          ctermfg = config_settings.color.cterm,
+      callback = function(_)
+        vim.api.nvim_set_hl(0, "SupermavenSuggestion", {
+          fg = M.config.color.suggestion_color,
+          ctermfg = M.config.color.cterm,
         })
         completion_preview.suggestion_group = "SupermavenSuggestion"
       end
