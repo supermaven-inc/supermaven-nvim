1af756eb7f5967078d4fdf59478b515e95818989
AlejandroSuero <asuerome@gmail.com>
2024-05-17 03:15:33 +0200

    feat: added config annotations and `color` entry

--- lua/supermaven-nvim/config.lua
+++ lua/supermaven-nvim/config.lua
@@ -1,9 +1,16 @@
+---@class SupermavenConfig
+---@field log_level LogLevel
+---@field ignore_filetypes string[]
 local default_config = {
 	keymaps = {
 		accept_suggestion = "<Tab>",
 		clear_suggestion = "<C-]>",
 		accept_word = "<C-j>",
 	},
+  color = {
+    suggestion_color = "#ffffff",
+    cterm_color = 244,
+  },
 	ignore_filetypes = {},
 	debug = false,
 	silence_info = false,
@@ -13,8 +20,12 @@ local default_config = {
 
 local M = {}
 
-local config = vim.deepcopy(default_config)
+local config = default_config
 
+--- Get the type of the value
+---@param key string: key in the config table
+---@return string: type of the value
+---@return boolean: whether the value is optional
 local wanted_type = function(key)
 	if vim.startswith(key, "__") then
 		return "nil", true
@@ -22,6 +33,9 @@ local wanted_type = function(key)
 	return type(default_config[key]), true
 end
 
+--- Validate the config table
+---@param args table: config table
+---@return table: validated config table
 local validate_config = function(args)
 	local to_check, validated = {}, {}
 
@@ -39,18 +53,29 @@ local validate_config = function(args)
 	return validated
 end
 
+---@class SupermavenConfig
+---@field get_config function: SupermavenConfig -> table
+--- Get the config table
+---@return SupermavenConfig: config table
 M.get_config = function()
 	return config
 end
 
+--- Set the config table
+---@param new_config SupermavenConfig: config table
 M.__set = function(new_config)
 	config = vim.tbl_deep_extend("force", {}, config, new_config or {})
 end
 
+--- Reset the config table
+--- This is useful when you want to change the config table
+--- without having to restart Neovim.
 M.reset_config = function()
-	config = vim.deepcopy(default_config)
+	config = default_config
 end
 
+--- Set the config table
+---@param args SupermavenConfig: config table
 M.setup_config = function(args)
 	if config.__setup then
 		return
