ff41221905056630647645c0937285cbb29b1dee
AlejandroSuero <asuerome@gmail.com>
2024-05-17 02:57:20 +0200

    feat(#18): made changes according to new config

--- lua/supermaven-nvim/init.lua
+++ lua/supermaven-nvim/init.lua
@@ -4,38 +4,63 @@ local u = require("supermaven-nvim.util")
 local listener = require("supermaven-nvim.document_listener")
 local config = require("supermaven-nvim.config")
 
-M = {}
+local M = {}
 
 M.setup = function(args)
-  local config_settings = config.setup_config(args)
-  if config_settings.keymaps.accept_suggestion ~= nil then
-    local accept_suggestion_key = config_settings.keymaps.accept_suggestion
-    vim.keymap.set('i', accept_suggestion_key, completion_preview.on_accept_suggestion, { noremap = true, silent = true })
-  end
+	if config.get_config().__setup then
+		return
+	end
 
-  if config_settings.keymaps.accept_word ~= nil then
-    local accept_word_key = config_settings.keymaps.accept_word
-    vim.keymap.set('i', accept_word_key, completion_preview.on_accept_suggestion_word, { noremap = true, silent = true })
-  end
+	args = args or {}
+	config.setup_config(args)
+	config = config.get_config()
+	if config.keymaps.accept_suggestion ~= nil then
+		local accept_suggestion_key = config.keymaps.accept_suggestion
+		vim.keymap.set(
+			"i",
+			accept_suggestion_key,
+			completion_preview.on_accept_suggestion,
+			{ noremap = true, silent = true }
+		)
+	end
 
-  if config_settings.keymaps.clear_suggestion ~= nil then
-    local clear_suggestion_key = config_settings.keymaps.clear_suggestion
-    vim.keymap.set('i', clear_suggestion_key, completion_preview.on_dispose_inlay, { noremap = true, silent = true })
-  end
-  binary:start_binary(config_settings.ignore_filetypes or {})
+	if config.keymaps.accept_word ~= nil then
+		local accept_word_key = config.keymaps.accept_word
+		vim.keymap.set(
+			"i",
+			accept_word_key,
+			completion_preview.on_accept_suggestion_word,
+			{ noremap = true, silent = true }
+		)
+	end
 
-  if config_settings.color and config_settings.color.suggestion_color and config_settings.color.cterm then
-    vim.api.nvim_create_autocmd({"VimEnter", "ColorScheme"}, {
-      pattern = "*",
-      callback = function(event)
-        vim.api.nvim_set_hl(0, "SupermavenSuggestion", { 
-          fg = config_settings.color.suggestion_color,
-          ctermfg = config_settings.color.cterm,
-        })
-        completion_preview.suggestion_group = "SupermavenSuggestion"
-      end
-    })
-  end
+	if config.keymaps.clear_suggestion ~= nil then
+		local clear_suggestion_key = config.keymaps.clear_suggestion
+		vim.keymap.set(
+			"i",
+			clear_suggestion_key,
+			completion_preview.on_dispose_inlay,
+			{ noremap = true, silent = true }
+		)
+	end
+	binary:start_binary(config.ignore_filetypes or {})
+
+	if config.color and config.color.suggestion_color and config.color.cterm then
+		vim.api.nvim_create_autocmd({ "VimEnter", "ColorScheme" }, {
+			pattern = "*",
+			callback = function(event)
+				vim.api.nvim_set_hl(0, "SupermavenSuggestion", {
+					fg = config.color.suggestion_color,
+					ctermfg = config.color.cterm,
+				})
+				completion_preview.suggestion_group = "SupermavenSuggestion"
+			end,
+		})
+	end
+
+	vim.api.nvim_create_user_command("SupermavenShowLog", function()
+		vim.cmd(string.format(":e %s", require("supermaven-nvim.logger"):get_log_path()))
+	end, {})
 end
 
 return M
